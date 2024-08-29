27ba629338974283dceaafb41e76b1d11376ad7a
AmitMotaphale <amitmotaphale01@gmail.com>
2024-08-29 14:41:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/userapp/PDFViewerActivity.java
@@ -0,0 +1,17 @@
+package com.example.userapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.File;
+
+public class PDFViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdfviewer);
+
+        // Initialize the PdfViewer
+
+    }
+}
---
+++ app/src/main/java/com/example/userapp/ebook/EbookAdapter.java
@@ -0,0 +1,93 @@
+package com.example.userapp.ebook;
+
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.userapp.PDFViewerActivity;
+import com.example.userapp.R;
+
+import java.util.List;
+
+public class EbookAdapter extends RecyclerView.Adapter<EbookAdapter.EbookViewHolder> {
+
+    private Context context;
+    private List<EbookData> list;
+    private DownloadManager downloadManager;
+
+    public EbookAdapter(Context context, List<EbookData> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public EbookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.ebook_item_layout,parent,false);
+        return new EbookViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EbookViewHolder holder, @SuppressLint("RecyclerView") int position) {
+
+        holder.ebookName.setText(list.get(position).getName());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, PDFViewerActivity.class);
+                intent.putExtra("pdfUrl",list.get(position).getUrl());
+                context.startActivity(intent);
+            }
+        });
+
+        holder.ebookDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pdfUrl = list.get(position).getUrl();
+
+                try {
+                    Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(pdfUrl));
+                    context.startActivity(myIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(context, "No application can handle this request."
+                            + " Please install a webbrowser",  Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class EbookViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView ebookName;
+        private ImageView ebookDownload;
+
+        public EbookViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ebookName = itemView.findViewById(R.id.ebookName);
+            ebookDownload = itemView.findViewById(R.id.ebookDownload);
+        }
+    }
+}
